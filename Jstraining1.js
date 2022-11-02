27eacbf8673f7fdd4870198010bd4edb9c4f0b09
Carter Pierre <carterspierre@gmail.com>
2022-11-02 19:00:10 -0400

    js training 1 and 3

---
+++ Jstraining1.js
@@ -0,0 +1,8 @@
+// 8 kyu
+// Training JS #1: create your first JS function and print "Hello World!"
+// refer to the example and write your first JS function
+
+function helloWorld() {
+    const str = 'Hello World!'
+    console.log(str)
+}
---
+++ Jstraining3.js
@@ -0,0 +1,44 @@
+// 8 kyu
+// Training JS #3: Basic data types--String
+
+// Task
+// Please refer to two example above and write your first JS function.
+
+// mission 1:
+// use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+// mission 2:
+// use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+// mission 3:
+// type the console.log() in the next line (don't forget to put the str in the parentheses).
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+// In the end, click "Submit" to submit your code pass this kata.
+
+var a1 = "A", a2 = "a", b1 = "B", b2 = "b", c1 = "C", c2 = "c", d1 = "D", d2 = "d", e1 = "E", e2 = "e", n1 = "N", n2 = "n"
+function Dad() {
+    //select some variable to combine "Dad"
+    return d1 + a2 + d2;
+}
+function Bee() {
+    //select some variable to combine "Bee"
+    return b1 + e2 + e2;
+}
+function banana() {
+    //select some variable to combine "banana"
+    return b2 + a2 + n2 + a2 + n2 + a2;
+}
+
+//answer some questions if you finished works above
+function answer1() {
+    //the answer should be "yes" or "no"
+    return "no";
+}
+function answer2() {
+    //the answer should be "yes" or "no"
+    return "no";
+}
+function answer3() {
+    //the answer should be "yes" or "no"
+    return "yes";
+}
