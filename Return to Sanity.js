8510cf30684f30a523c3f94c8ea5ae64af68b960
Carter Pierre <carterspierre@gmail.com>
2022-12-06 20:34:43 -0500

    2 commits

---
+++ Return to Sanity.js
@@ -0,0 +1,11 @@
+// 8 kyu
+// Return to Sanity
+
+// DESCRIPTION:
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+function mystery() {
+    var results =
+        { sanity: 'Hello' };
+    return results;
+}
